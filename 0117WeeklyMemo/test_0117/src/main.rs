8dff06227b3cefe391fff55ba7fea9714d5a636b
Shigi-p <shigi.522@gmail.com>
2022-01-28 18:07:18 +0900

    [add] 0117Memo追加
    後からcommitしているのでコミットが正しいのかどうかは謎

---
+++ 0117WeeklyMemo/test_0117/src/main.rs
@@ -0,0 +1,13 @@
+use test_0117::conversions;
+// use test_0117::types;
+
+fn main() {
+    println!("Hello, world!");
+
+    // ----- types
+    // types::casting_test();
+    // types::literals_test();
+
+    // ----- conversions
+    conversions::from_and_into_test();
+}
