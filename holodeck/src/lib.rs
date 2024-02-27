5727f7e010163224fbe01ed1b92dc830ecb3267c
Shigi-p <shigi.522@gmail.com>
2024-02-28 01:13:19 +0900

    [add] (holodeck) add REST Api sample

---
+++ holodeck/src/lib.rs
@@ -0,0 +1,41 @@
+// https://dev.to/rogertorres/rest-api-with-rust-warp-1-introduction-342e
+
+#[cfg(test)]
+mod tests {
+    use warp::http::StatusCode;
+    use warp::test::request;
+    use super::filters;
+
+    #[tokio::test]
+    async fn try_list() {
+        let api = filters::list();
+
+        let response = request()
+            .method("GET")
+            .path("/holodeck")
+            .reply(&api)
+            .await;
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+}
+
+mod filters{
+    use warp::Filter;
+    use super::handlers;
+
+    pub fn list() ->  impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone{ 
+        warp::path!("holodeck")
+            .and(warp::get())
+            .and_then(handlers::handle_list)
+    }
+}
+
+mod handlers{
+    use warp::http::StatusCode;
+    use std::convert::Infallible;
+
+    pub async fn handle_list() -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::OK)
+    }
+}
